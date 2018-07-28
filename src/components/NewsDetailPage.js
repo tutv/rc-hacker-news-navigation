d66149e58d280ccc19acfeff1e809670df00c648
Max <tutv95@gmail.com>
2018-07-28 17:03:30 +0700

    Updated header news detail

--- src/components/NewsDetailPage.js
+++ src/components/NewsDetailPage.js
@@ -2,6 +2,16 @@ import React, {Component} from 'react'
 import NewsDetail from "./NewsDetail"
 
 class NewsDetailPage extends Component {
+    static navigationOptions = {
+        title: 'News',
+        headerStyle: {
+            backgroundColor: '#ff6600'
+        },
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+
     render() {
         const itemId = this.props.navigation.getParam('id')
 
