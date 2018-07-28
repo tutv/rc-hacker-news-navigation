8cf6ebaa1bb7fc94b85411ad0bc2e8494adf36ae
Max <tutv95@gmail.com>
2018-07-28 14:07:46 +0700

    init

---
+++ src/components/HomePage.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import {View, StyleSheet} from 'react-native'
+
+class HomePage extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
+
+export default HomePage
